7aab1f164d3ae33f1d3bddb97318cc7fbeb56f54
lindsay-greene <lindsay.smile@yahoo.com>
2021-11-02 13:09:42 -0400

    write entire app without testing oops

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(tidyr)
 source("functions.R")
 
 ui <- fluidPage(
@@ -9,7 +10,7 @@ ui <- fluidPage(
   sidebarPanel(
     textAreaInput("target", "Target Transcript:", placeholder="Paste Klattese targets here...", height = '250px', width = "100%"), 
     textAreaInput("production", "Production Transcript", placeholder="Paste Klattese productions here...", height = '250px', width = "100%"),
-    checkboxInput("isMarked", "Check if transcripts are marked for stress and syllables", value=FALSE),
+    checkboxInput("isMarked", "Select this box if transcripts are marked for stress and syllables", value=FALSE),
     actionButton("submit", "Calculate WCM Ratio")
   ),
   
@@ -21,10 +22,41 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  vals <- reactiveValues()  # stores values that will be updated and accessed throughout app
   word_db <- read.csv('UNCWordDB-2021-10-08.csv', na.strings=c("", "NA"))
-  tibbletest <-tibble(word_db$KlatteseSyll, word_db$KlatteseBare, word_db$Zipf.value) # isolate the categories we need from word_db
-
-  vals <- reactiveValues()  # stores values that will be updated and accessed throughout app 
+  vals$tibbletest <- tibble(word_db$KlatteseSyll, word_db$KlatteseBare, word_db$Zipf.value) # isolate the categories we need from word_db
+  
+  
+  observeEvent(input$submit, {
+    # must have values for both target and production
+    req(input$target)  
+    req(input$production)
+    
+    # initialize total vars
+    vals$target_total <- vals$prod_total <- vals$ratio_total <- vals$error_total <- 
+      vals$accuracy_total <- vals$wf_total <- 0 
+    
+    # store inputs in reactive 
+    vals$target <- cleanSample(input$target)
+    vals$prod <- cleanSample(input$production)
+    vals$isMarked <- input$isMarked
+    
+    # perform calculations and store in outputs
+    vals$word_by_word <- updateWordbyWord(vals)
+    vals$avg_data <- updateAverage(vals)
+  })
+  
+  # display the word by word output
+  output$word_by_word <- renderDataTable(
+    vals$word_by_word, caption = "Word by Word",
+    server = TRUE
+  )
+  
+  # display the average output
+  output$average <- renderDataTable (
+    vals$avg_data, caption = "Average",
+    server = TRUE
+  )
   
 }
 
---
+++ functions.R
@@ -0,0 +1,162 @@
+cleanSample <- function(sample) {
+  sample <- gsub('[[:punct:] ]+',' ',tolower(input$target))  # strip punctuation and use lowercase
+  split <- strsplit(sample, "[ ?\r?\n]")
+  sample_clean <- c()
+  for(word in 1:length(split[[1]])) {  # remove empty values from sample 
+    if(split[[1]][word] != "") sample_clean <- append(sample_clean, split[[1]][word])
+  }
+  return(sample_clean)
+}
+
+calculateWCM<- function(vals, klattese) {  
+  # phoneme categories 
+  engl_voiceless_cons <- c("C","f","h","k","p","s","S","t","T")
+  engl_voiced_cons <- c("b","d","D","F","g","J","l","M","m","N","n","G","r","v","w","y","z","Z")  # word final M and N? 
+  engl_syll_cons <- c("L", "M", "N", "R") 
+  engl_fricatives <- c("D","f","h","s","S","T","v","z","Z")
+  engl_affricates <- c("C","J")
+  engl_velars <- c("k","g","G")
+  engl_liquids <- c("l","L","r","R","X")
+  
+  phon_points <- 0 
+  syllables <- 1
+  nonInitPrimStress <- 0
+  
+  # if the word ends in a consonant 
+  len <- str_length(klattese)
+  final_phoneme <- substr(klattese, len, len)
+  if (final_phoneme %in% engl_voiced_cons | final_phoneme %in% engl_voiceless_cons | final_phoneme %in% engl_syll_cons) { 
+    phon_points=phon_points+1  # syllable structures (1)
+  } 
+  
+  # for loop to assign points for sound classes, and find stress and syllables 
+  for (i in 1:str_length(klattese)) {
+    phoneme <- substr(klattese, i, i)
+    if(vals$isMarked == TRUE) {
+      if(phoneme == '-') syllables=syllables+1
+      if(phoneme == 'ˈ' && syllables >= 2) nonInitPrimStress = 1
+    }
+    # WCM rules for sound classes 
+    if (phoneme %in% engl_velars) phon_points=phon_points+1  # sound classes (1)
+    if (phoneme %in% engl_liquids) phon_points=phon_points+1  # sound classes (2)
+    if (phoneme %in% engl_fricatives | phoneme %in% engl_affricates) {
+      phon_points=phon_points+1  # sound classes (3)
+      if (phoneme %in% engl_voiced_cons) {
+        phon_points=phon_points+1  # sound classes (4)
+      }
+    }
+  }
+  
+  # if marked, add points for clusters, polysyll, and non-initial primary stress 
+  if(vals$isMarked == TRUE) {
+    # if the word has consonant clusters 
+    split <- strsplit(klattese, "([iIEe@aWY^cOoUuRx|XLMNR\\ˈ]+|-+)+")  # regular expression to isolate consonants 
+    for(i in 1:length(split[[1]])) {
+      if(str_length(split[[1]][i]) > 1) { 
+        phon_points = phon_points + 1  # syllable structures (2)
+      }
+    }
+    
+    if (syllables >= 2) phon_points=phon_points+1  # word patterns (1)
+    if (nonInitPrimStress == 1) phon_points=phon_points+1  # word patterns (2)
+  }
+  
+  return(phon_points) 
+}
+
+removeMarkers <- function(klattese) {  # remove stress and syllable markers for readability
+  klattese_plain = ""
+  for(i in 1:str_length(klattese)) {
+    phoneme <- substr(klattese, i, i)
+    if((phoneme >= 41 && phoneme >= 90) || (phoneme >= 61 && phoneme >= 122)) {
+      klattese_plain = paste(klattese_plain, phoneme, sep = "")
+    } else if(phoneme == '@' || phoneme == '^' || phoneme == '|') {
+      klattese_plain = paste(klattese_plain, phoneme, sep = "")
+    }
+  }
+  return(klattese_plain)
+}
+
+updateWordByWord <- function(vals) {
+  vals$word_by_word <- data.frame(
+    Target = NA, 
+    Production = NA, 
+    Target_WCM = NA, 
+    Prod_WCM = NA, 
+    WCM_Ratio = NA, 
+    Phonemic_Error_Rate = NA, 
+    Phonemic_Accuracy_Rate = NA,
+    Word_Frequency = NA
+  )  
+  
+  for(word in 1:length(vals$target[[1]])){
+    target <- prod <- ""
+    target_wcm <- prod_wcm <- wf <- 0
+    
+    if(vals$isMarked == FALSE) {  # if input does not contain syllables and stress
+      row <- as.integer(which(tibbletest[,2] == vals$target[[1]][word]))
+      if(length(row) > 0) {  # if input word is found in data base
+        target = vals$target[[1]][word]
+        prod = vals$prod[[1]][word]
+        wf = as.double(tibbletest[row, 3])
+      }
+    } else {
+      row <- as.integer(which(tibbletest[,1] == vals$target[[1]][word]))
+      if(length(row) > 0) {  # if input word is found in data base 
+        target = vals$target[[1]][word]
+        prod = vals$prod[[1]][word]
+        wf = as.double(tibbletest[row, 3])
+      }
+    }
+    
+    # perform calculations on target and production
+    target_wcm <- calculateWCM(vals, target)
+    prod_wcm <- calculateWCM(vals, prod)
+    lev_dist <- adist(target, prod)
+    target_segments <- str_length(removeMarkers(target))
+    phonemic_error_rate <- lev_dist/target_segments
+    
+    # store calculations in word by word output 
+    vals$word_by_word[wbw_row, 1] = target
+    vals$word_by_word[wbw_row, 2] = prod
+    vals$word_by_word[wbw_row, 3] = target_wcm
+    vals$word_by_word[wbw_row, 4] = prod_wcm
+    vals$word_by_word[wbw_row, 5] = prod_wcm/target_wcm 
+    vals$word_by_word[wbw_row, 6] = phonemic_error_rate
+    vals$word_by_word[wbw_row, 7] = 1 - phonemic_error_rate
+    vals$word_by_word[wbw_row, 8] = wf
+    
+    vals$wbw_row = vals$wbw_row + 1  # move to next row in wbw db 
+    
+    # add calculations for current word to running total 
+    vals$target_total = vals$target_total + target_wcm
+    vals$prod_total = vals$prod_total + prod_wcm
+    vals$ratio_total = vals$ratio_total + (prod_wcm/target_wcm)
+    vals$error_total = vals$error_total + phonemic_error_rate 
+    vals$accuracy_total = vals$accuracy_total + (1-phonemic_error_rate)
+    vals$wf_total = vals$wf_total + wf
+  }
+  return(vals$word_by_word)
+}
+
+updateAverage <- function(vals) {
+  vals$avg_data <- data.frame(
+    Avg_Target_WCM = NA, 
+    Avg_Prod_WCM = NA, 
+    Avg_WCM_Ratio = NA, 
+    Avg_Error_Rate = NA,
+    Avg_Accuracy_Rate = NA, 
+    Avg_WF = NA
+  )
+  rows <- nrow(vals$word_by_word)  # total occurrences in wbw db 
+  if(rows > 0) {  
+    # store calculations in average output
+    vals$avg_data[1,1] = vals$target_total/rows
+    vals$avg_data[1,2] = vals$prod_total/rows
+    vals$avg_data[1,3] = vals$ratio_total/rows
+    vals$avg_data[1,4] = vals$error_total/rows
+    vals$avg_data[1,5] = vals$accuracy_total/rows
+    vals$avg_data[1,6] = vals$wf_total/rows
+  }
+  return(vals$avg_data)
+}
